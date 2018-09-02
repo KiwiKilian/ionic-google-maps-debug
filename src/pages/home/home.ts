227e4d845567d619817b9e9108ec06e8eefa3a41
Kilian Finger <kilianfinger@gmail.com>
2018-09-02 12:56:47 +0200

    Test v2.2.1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -32,39 +32,38 @@ export class HomePage {
         const mapOptions: GoogleMapOptions = {
             camera: {
                 target: position,
-                zoom: 18,
+                zoom: 10,
             }
         };
 
         this.map = GoogleMaps.create('map_canvas', mapOptions);
 
-
-        this.map.addMarkerSync({
-            title: 'Actual Position',
-            icon: 'blue',
-            position,
-        });
-
-        this.map.addMarkerSync({
-            title: 'Right Anchor Position',
-            icon: {
-                url: './assets/imgs/arrow-green.png',
-                // Anchor also accepts: [0, 0]
-                anchor: {x: 0, y: 0},
-            },
-            position,
-
-        });
-
-        this.map.addMarkerSync({
-            title: 'False Anchor Position',
-            icon: {
-                url: './assets/imgs/arrow-red.png',
-            },
-            position,
-            // Will only accept number[]
-            anchor: [0, 0],
-        });
+        // this.map.addMarkerSync({
+        //     title: 'Actual Position',
+        //     icon: 'blue',
+        //     position,
+        // });
+        //
+        // this.map.addMarkerSync({
+        //     title: 'Right Anchor Position',
+        //     icon: {
+        //         url: './assets/imgs/arrow-green.png',
+        //         // Anchor also accepts: [0, 0]
+        //         anchor: {x: 0, y: 0},
+        //     },
+        //     position,
+        //
+        // });
+        //
+        // this.map.addMarkerSync({
+        //     title: 'False Anchor Position',
+        //     icon: {
+        //         url: './assets/imgs/arrow-red.png',
+        //     },
+        //     position,
+        //     // Will only accept number[]
+        //     anchor: [0, 0],
+        // });
     }
 
     increaseUpper() {
